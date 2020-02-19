6efe32b5eb54e09b247f17c97e8f99ff6fb59118
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-19 06:38:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,15 @@
+def key_for_min_value(hash)
+    lowest_possible_value = 0
+    lowest_key = nil 
+
+    hash.each do |key, value|
+        if lowest_possible_value == 0 || value < lowest_possible_value
+            lowest_possible_value = value
+            lowest_key = key
+        end
+    end
+    lowest_key
+end
 
 
 
